f361701f0b68e2f1443038301b9ab82ce4469cfc
Eugene <snegur.1998@gmail.com>
2022-07-12 16:54:57 +0300

    Add js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  let elements = document.querySelectorAll('.item');
+  elements.forEach((elem, index) =>
+    index % 2 == 0 ? (elem.style.background = 'red') : elem
+  );
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  const firstElement = document.querySelector('.item');
+  let nextItem = firstElement.nextElementSibling;
+  for (let i = 2; i < document.querySelectorAll('.item').length; i++) {
+    nextItem.classList.contains('likedItem')
+      ? (nextItem.style.background = 'blue')
+      : (nextItem = nextItem.nextElementSibling);
+  }
+};
